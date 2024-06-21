08cf45dbda6b07ed38cb214165e3d52328eb1da9
Duarlo <duarlo@yandex.com>
2024-06-20 18:17:29 -0600

    prueba tecnica

--- src/persona/datospersonas.java
+++ src/persona/datospersonas.java
@@ -1,5 +1,50 @@
 package persona;
 
-public class datospersonas {
-
+public class datospersonas extends persona{
+	
+	public class void main(args String[]) {
+		
+		String datos= "jhon-Hotmail-22/04/1997-doe-H+katty-Gmail-05/08/2002-taylor-M+james-Yahoo-14/02/2005-green-H+allison-Hotmail-25/12/1980-parker-M";
+		Persona persona =new Persona();
+		
+			int i=0;
+			do {
+			do {
+				setNombreCompleto=datos.charAt(i).concat(setNombreCompleto);
+				i++;
+			}
+			while(datos.charAt(i)!='-');
+			do {
+				setCorreo=datos.charAt(i).concat(setCorreo);
+				i++;
+			}
+			while(datos.charAt(i)!='-');
+			do {
+				setDate=datos.charAt(i).concat(setDate);
+				i++;
+			}
+			while(datos.charAt(i)!='-');
+			do {
+				setNombrecompleto=datos.charAt(i).concat(setNombreCompleto);
+				i++;
+			}
+			while(datos.charAt(i)!='-');
+			do {
+				setGenero=datos.charAt(i).concat(setGenero);
+				i++;
+			}
+			while(datos.charAt(i)!='-');
+			}
+			while(datos.charAt(i)!='+');
+			
+			setNombrecompleto=datos.
+		}
+	
+	}
+	
+	
+	
+	
+	
+	
 }
--- src/persona/persona.java
+++ src/persona/persona.java
@@ -1,5 +1,66 @@
 package persona;
 
+import java.util.Date;
+
 public class persona {
 
+	private String NombreCompleto;
+	private int Edad;
+	private char Genero;
+	private Date FechaNacimiento;
+	private String Correo;
+	private Long id;	
+	
+	public class persona() {
+		
+	}
+	
+	public class persona(String NombreCompleto,int Edad,char Genero,Date FechaNacimiento,String Correo,Long id ){
+		this.NombreCompleto=nombrecompleto;
+		this.Edad=edad;
+		this.Genero=genero;
+		this.fechaNacimiento=fechanacimiento;
+		this.Correo=correo;
+		this.id=id;
+	}
+	public String getNombreCompleto() {
+		return NombreCompleto;
+	}
+	public void setNombreCompleto(String nombreCompleto) {
+		NombreCompleto = nombreCompleto;
+	}
+	public int getEdad() {
+		return Edad;
+	}
+	public void setEdad(int edad) {
+		Edad = edad;
+	}
+	public char getGenero() {
+		return Genero;
+	}
+	public void setGenero(char genero) {
+		Genero = genero;
+	}
+	public char getDate() {
+		return Date;
+	}
+	public void setDate(char date) {
+		Date = date;
+	}
+	public String getCorreo() {
+		return Correo;
+	}
+	public void setCorreo(String correo) {
+		Correo = correo;
+	}
+	public String getLong() {
+		return Long;
+	}
+	public void setLong(String l) {
+		Long = l;
+	}
+	
+	
+	
+	
 }
